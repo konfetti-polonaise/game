bb8edc58a2dade19fd2eb69f895b68f24d3471d2
Majus Lenz <mariuslenzing@yahoo.de>
2015-12-14 22:57:50 +0100

    Steuerung mit W A S D

--- js/Snake.js
+++ js/Snake.js
@@ -4,7 +4,7 @@ var Snake = (function () {
     var cls = function () {
         var speed = 7;      // Initialgeschwindigkeit
         var buff = null;    // Powerupeffekt
-        var head = new Head(16 + 32, 16 + 32);  // Startposition. TODO: Zufällig platzieren ???
+        var head = new Head(16 + 32, 16 + 32);  // Startposition.
         var passingList = [];   //Dancer die nach hinten durchgereicht werden.
 
         // Richtung in die sich die Richtung des Heads ändern wird, sobald Schlange im Grid ist.
