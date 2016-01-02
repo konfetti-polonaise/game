7280b9047f458637eac31120168eaa27cb78a34b
Majus Lenz <mariuslenzing@yahoo.de>
2016-01-02 23:43:19 +0100

    Bug behoben, dass bei einem GameOver die GameStage nicht mehr ausgegraut im Hintergrund sichbar war.

--- js/GameOver.js
+++ js/GameOver.js
@@ -13,6 +13,10 @@ var GameOver = (function () {
         // HTML anpassung
         document.getElementById("gameover").className = "display";
         document.getElementById("final-score").innerHTML = Score.getScore();
+
+        // Keine Ahnung was diese Zeile macht, aber ohne sie funktionierts es nicht,
+        // dass die GameStage weiterhin im Hintergrund zu sehen ist.
+        text.events.onInputDown.add(cls.restartGame, this);
     };
 
     cls.restartGame = function() {
